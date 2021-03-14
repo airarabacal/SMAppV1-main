5e8daec174bc68d0ff047e971a7d42d677a2c24d
airarabacal <rabacalaira@gmail.com>
2021-03-14 17:48:43 +0800

    my first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import TopAppBar from "./TopAppBar";
+import FullWidthTabs from "./TabPages";
+
+function App() {
+   
+    return (
+      <div>
+        <TopAppBar />       
+        <FullWidthTabs /> 
+      </div>
+    );
+  }
+  
+  export default App;
